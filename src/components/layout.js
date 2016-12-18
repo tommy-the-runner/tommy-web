b088cead73f1732cda0e80dfaa728773853f0529
ertrzyiks <mateusz.derks@gmail.com>
2016-12-18 09:48:09 +0100

    Enable lint on CI

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,14 +2,14 @@ import serialize from 'serialize-javascript'
 import {html, head, title, body, div, script, hJSX} from '@cycle/dom'
 
 module.exports = function wrapVTreeWithHTMLBoilerplate({canonicalUrl, vtree, context, config, clientBundle}) {
-    const baseTitle = "Tommy the Runner"
-    const extraTitle = ` - ${context.title}`
-    const title = `${baseTitle}${extraTitle}`
+  const baseTitle = 'Tommy the Runner'
+  const extraTitle = ` - ${context.title}`
+  const titleText = `${baseTitle}${extraTitle}`
 
-    const jsBundle = `/assets/${clientBundle['js/bundle.js']}`
-    const cssBundle = `/assets/${clientBundle['css/styles.css']}`
+  const jsBundle = `/assets/${clientBundle['js/bundle.js']}`
+  const cssBundle = `/assets/${clientBundle['css/styles.css']}`
 
-    return (
+  return (
       <html lang="en">
       <head>
         <link href="/assets/css/reset.css" rel="stylesheet" type="text/css" />
@@ -17,16 +17,16 @@ module.exports = function wrapVTreeWithHTMLBoilerplate({canonicalUrl, vtree, con
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css' />
         <link href={ config.base_url + '/assets/images/tommy-ico.png' } rel="icon" />
         <meta charset="UTF-8" />
-        <title>{title}</title>
+        <title>{titleText}</title>
         <meta name="description" content="Exercise your testing skills with a coding challenge." />
 
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content={title} />
+        <meta name="twitter:title" content={titleText} />
         <meta name="twitter:description" content="Exercise your testing skills with a coding challenge." />
         <meta name="twitter:creator" content="@ertrzyiks" />
         <meta name="twitter:image" content={ config.base_url + '/assets/images/tommy-image.png' } />
 
-        <meta property="og:title" content={title} />
+        <meta property="og:title" content={titleText} />
         <meta property="og:type" content="website" />
         <meta property="og:url" content={ canonicalUrl } />
         <meta property="og:image" content={ config.base_url + '/assets/images/tommy-image-fb.png' } />
--- src/components/specs_panel.js
+++ src/components/specs_panel.js
@@ -4,36 +4,36 @@ import AceEditor from 'cyclejs-ace-editor'
 import {Observable, Subject} from 'rx'
 
 const defaultEditorOptions = {
-    theme: 'ace/theme/clouds_midnight',
-    readOnly: true,
-    fontSize: 13,
-    sessionOptions: {
-        mode: 'ace/mode/javascript',
-        tabSize: 2
-    }
+  theme: 'ace/theme/clouds_midnight',
+  readOnly: true,
+  fontSize: 13,
+  sessionOptions: {
+    mode: 'ace/mode/javascript',
+    tabSize: 2
+  }
 }
 
 function intent({context}) {
-    const specCodeValue$ = context.map(json => {
-        const specsCodeRaw = json.specsCode || ''
-        return specsCodeRaw.replace('require(\'subject\')', '/* your code here */')
-    })
+  const specCodeValue$ = context.map(json => {
+    const specsCodeRaw = json.specsCode || ''
+    return specsCodeRaw.replace('require(\'subject\')', '/* your code here */')
+  })
 
-    const specCodeEditable$ = context.map(json => {
-        if (json.specsCodeEditable === false) {
-            return false
-        }
-        return true
-    })
-
-    return {
-        specCodeValue$,
-        specCodeEditable$
+  const specCodeEditable$ = context.map(json => {
+    if (json.specsCodeEditable === false) {
+      return false
     }
+    return true
+  })
+
+  return {
+    specCodeValue$,
+    specCodeEditable$
+  }
 }
 
 function view(editor) {
-    return Observable.just(
+  return Observable.just(
         <div id="spec">
             <header className="clearfix">
                 <div className="container-header">
@@ -48,29 +48,29 @@ function view(editor) {
 }
 
 function SpecsPanel(sources) {
-    const {DOM} = sources
-    const {specCodeValue$, specCodeEditable$} = intent(sources)
+  const {DOM} = sources
+  const {specCodeValue$, specCodeEditable$} = intent(sources)
 
-    const params$ = specCodeEditable$.map(isEditable => {
-        return Object.assign({}, defaultEditorOptions, {
-            readOnly: !isEditable
-        })
+  const params$ = specCodeEditable$.map(isEditable => {
+    return Object.assign({}, defaultEditorOptions, {
+      readOnly: !isEditable
     })
-    const editor = AceEditor({DOM, initialValue$: specCodeValue$, params$})
-    const vtree$ = view(editor)
+  })
+  const editor = AceEditor({DOM, initialValue$: specCodeValue$, params$})
+  const vtree$ = view(editor)
 
-    const code$ = editor.value$.map(sources => {
-        return sources.replace('/* your code here */', 'require(\'subject\')')
-    })
+  const code$ = editor.value$.map(code => {
+    return code.replace('/* your code here */', 'require(\'subject\')')
+  })
 
-    return {
-        DOM: vtree$,
-        code$: code$
-    }
+  return {
+    DOM: vtree$,
+    code$: code$
+  }
 }
 
 function SpecsPanelWrapper(sources) {
-    return isolate(SpecsPanel)(sources)
+  return isolate(SpecsPanel)(sources)
 }
 
 export default SpecsPanelWrapper
